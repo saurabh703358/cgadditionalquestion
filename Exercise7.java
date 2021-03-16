fd0b3be868a9cc20df9f1c9ea52377f64cf40e33
saurabh703358 <78597574+saurabh703358@users.noreply.github.com>
2021-03-16 22:14:25 +0530

    Updated Solution

---
+++ Exercise7.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Exercise7 {
+
+	public static void main(String[] args) {
+
+		Map<Integer, Integer> m1 = new HashMap<Integer, Integer>();
+		m1.put(1, 4);
+		m1.put(2, 6);
+		m1.put(4, 7);
+		m1.put(5, 9);
+		System.out.println(avgValuesOfOddKeys(m1));
+	}
+
+	public static int avgValuesOfOddKeys(Map<Integer, Integer> m1) {
+
+		int l = 0, m = 0;
+
+		Iterator<Integer> i = m1.keySet().iterator();
+		while (i.hasNext()) {
+
+			int n = (Integer) i.next();
+
+			if (n % 2 != 0) {
+
+				m += m1.get(n);
+				l++;
+			}
+		}
+		return m / l;
+	}
+}
