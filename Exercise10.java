2da18627e4669bf42ff67306d808fc4cc7dbcaf5
saurabh703358 <78597574+saurabh703358@users.noreply.github.com>
2021-03-16 22:28:59 +0530

    Updated

---
+++ Exercise10.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Exercise10 {
+
+	public static void main(String[] args) {
+
+		int[] a = { 1, 2, 3, 4 };
+		int[] b = { 3, 4, 5, 6 };
+		System.out.println(mergeArray(a, b));
+	}
+
+	public static int mergeArray(int a[], int b[]) {
+
+		List<Integer> l1 = new ArrayList<Integer>();
+		List<Integer> l2 = new ArrayList<Integer>();
+
+		int i, d = 0;
+		for (i = 0; i < a.length; i++) {
+			l1.add(a[i]);
+		}
+
+		for (i = 0; i < b.length; i++) {
+			l2.add(b[i]);
+		}
+		l1.retainAll(l2);
+
+		for (i = 0; i < l1.size(); i++) {
+			d += (Integer) l1.get(i);
+		}
+		return d;
+	}
+}
